89dabac0a7c6c23cc244c6f65d959deebf2cf227
Vlemrc <lemerciervictor@gmail.com>
2025-08-19 11:57:38 +0200

    page contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
+
 export default function ContactPage() {
   return (
-    <div className="w-3/4 p-10">
-      <div className="text-white">
-        <h1 className="text-4xl font-bold mb-8">Contact</h1>
-        <p className="text-gray-300 text-lg leading-relaxed">
-          Vous pouvez me contacter à l&apos;adresse suivante : <a href="mailto:email@example.com" className="underline">email@example.com</a>
-        </p>
-      </div>
+    <div className="w-3/4 pl-20 flex flex-col h-full items-start justify-center text-white text-sm">
+      <p>Thanks for stopping by. <br /> I hope you like your visit here. This site reflects my path, <br /> exploring, growing, and leaving traces that matter.</p>
+      <h1 className="font-arges text-[200px]/[1.2] uppercase">let&apos;s connect</h1>
+      <p>Feel free to reach me by email at victorlemercier.dev@gmail.com <br />
+      Here’s my <Link href="https://www.linkedin.com/in/victorlemercier/" target="_blank">LinkedIn</Link></p>
+      <p>I am currently seeking a full-time position (CDI), <br /> available from October 2025.</p>
     </div>
   );
 }
