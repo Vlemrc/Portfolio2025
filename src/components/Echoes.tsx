ab87edceb1fb810d1db33a5ddf68211eb3bc52dc
Vlemrc <lemerciervictor@gmail.com>
2025-08-19 16:28:12 +0200

    Spotify iframe playlist

--- src/components/Echoes.tsx
+++ src/components/Echoes.tsx
@@ -2,32 +2,17 @@ export default function Echoes() {
     return (
         <>
             <h3 className="pt-12 uppercase text-[10px] font-bold pb-5">My echoes</h3>
-            <iframe 
-                data-testid="embed-iframe" 
-                style={{ borderRadius: "12px" }}
-                src="https://open.spotify.com/embed/track/2oKOGvdJ5BQEiguaSQJJpc?utm_source=generator&theme=0" 
-                width="100%" 
-                height="90"
-                className="w-2/3"
-                frameBorder="0" 
-                scrolling="no"
-                allowFullScreen={true}
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                loading="lazy">
-            </iframe>
             <iframe
                 data-testid="embed-iframe"
-                style={{ borderRadius: "12px" }}
-                src="https://open.spotify.com/embed/track/6NiRlK5sphGYxU4UPVlN4p?utm_source=generator&theme=0"
-                width="100%"
-                height="90"
-                className="w-2/3"
+                style={{ borderRadius: '12px' }}
+                src="https://open.spotify.com/embed/playlist/1uowCt1nschyYJbujear78?utm_source=generator"
+                width="70%"
+                height="200"
                 frameBorder="0"
-                scrolling="no"
-                allowFullScreen={true}
+                allowFullScreen
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="lazy"
-            />
+            ></iframe>
         </>
     )
 }
