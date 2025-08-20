8ef8fc3a25b40275480eea5fb071ef7fe4d58a10
Vlemrc <lemerciervictor@gmail.com>
2025-08-20 18:36:29 +0200

    path page

--- src/app/path/page.tsx
+++ src/app/path/page.tsx
@@ -1,19 +1,20 @@
+import AnimatedTitle from "@/components/AnimatedTitle"
 import Education from "@/components/Education"
 import Experiences from "@/components/Experiences"
+import Paragraph from "@/components/Paragraph"
 import Resume from "@/components/Resume"
 import SocialIcons from "@/components/SocialIcons"
 
 export default function PathPage() {
   return (
-    <div className="w-3/4 pl-20 pt-[100px] flex flex-row h-fit items-end justify-between pr-10">
+    <div className="w-3/4 pl-20 pt-[70px] flex flex-row h-fit items-end justify-between pr-10">
       <div className="text-white w-1/2">
-        <h1 className="font-arges text-[200px]/[1.2] uppercase">I&apos;m&nbsp;a&nbsp;spinosaur</h1>
-        <p className="text-gray-300 w-3/4 text-sm">
-          Millions of years of evolution… and I ended up making websites. Part designer, part developer, part dinosaur. 100% passionate about building great things.
-          Extinct? Maybe. Skilled? Definitely.
-        </p>
+        <AnimatedTitle text="I&apos;m&nbsp;a&nbsp;spinosaur" className="text-[200px]/[1.2] font-arges uppercase font-bold text-white mb-10" delay={50} />
+        <div className="text-gray-300 w-3/4 text-sm">
+          <Paragraph starttime={0.800} content="Millions of years of evolution… and I ended up making websites. Part designer, part developer, part dinosaur. 100% passionate about building great things. Extinct? Maybe. Skilled? Definitely." />
+        </div>
         <h3 className="pt-10 uppercase text-[10px] font-bold pb-5 text-white">TOOL STACK</h3>
-        <ul className="flex flex-col text-xs font-light mb-16">
+        <ul className="flex flex-col text-xs font-light mb-10">
           <li>PHP</li>
           <li>Javascript - TypeScript</li>
           <li>React - Next.js</li>
