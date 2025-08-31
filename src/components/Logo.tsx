371453337cd349e856230702c8d1d54a9b3e1c0d
Vlemrc <lemerciervictor@gmail.com>
2025-08-31 21:23:05 +0200

    add sound to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,12 +2,14 @@
 import { motion } from "framer-motion"
 import Link from "next/link"
 import useProjectStore from "@/stores/useProjectStore"
+import useNavClickSound from "@/lib/hooks/useNavClickSound"
 interface LogoProps {
   color?: string | undefined
 }
 
 export default function Logo({ color }: LogoProps) {
   const { activeProject, setActiveProject } = useProjectStore();
+  const playClick = useNavClickSound()
   const paths = [
     "M182.02,117h26.93l-27.32,273.95h2.34l56.58-273.95h26.93l-70.24,304.38h-37.85l22.63-304.38Z",
     "M278.02,117h27.32l-44.88,304.38h-27.32l44.88-304.38Z",
@@ -91,12 +93,17 @@ export default function Logo({ color }: LogoProps) {
     },
   }
 
+  const handleHomeClick = () => {
+    setActiveProject(null)
+    playClick()
+  }
+
   return (
     <Link
       className={`absolute z-10 top-6 left-6 flex items-center justify-center w-[200px] cursor-pointer ${color ? "opacity-70" : "opacity-100"} hover:opacity-100 transition-opacity duration-300 ease-in-out`}
       style={{ perspective: "1000px" }}
       href="/"
-      onClick={() => setActiveProject(null)}
+      onClick={handleHomeClick}
     >
       <motion.svg
         xmlns="http://www.w3.org/2000/svg"
