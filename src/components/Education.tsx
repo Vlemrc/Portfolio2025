b01bfc680fc62f7f187e18f9c62359e21c8ce96b
Vlemrc <lemerciervictor@gmail.com>
2025-09-02 13:29:05 +0200

    add Bac S

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -53,7 +53,7 @@ export default function Education() {
             </li>
             <li className="overflow-hidden h-[30px]">
               <div className={`${showExp3 ? "translate-y-0" : "-translate-y-12"} transition-transform duration-300 flex flex-row gap-8`}>
-                <p className="min-w-[58px] text-[10px]">2020 - Now</p>
+                <p className="min-w-[58px] text-[10px]">2019 - 2025</p>
                 <div>
                   <p className="uppercase text-[10px] font-bold">E-learning web development</p>
                   <Link href="https://dyma.fr/" target="_blank" className="flex flex-row items-end gap-0.5 group">
@@ -62,6 +62,15 @@ export default function Education() {
                           <path fill="#fff" d="M228.84,106.01L127.22,4.39c-.35-.35-.72-.68-1.1-1-.17-.14-.35-.26-.53-.4-.21-.16-.42-.33-.65-.48-.22-.14-.44-.27-.66-.4-.2-.12-.39-.24-.6-.35-.23-.12-.46-.22-.69-.33-.21-.1-.42-.21-.64-.3-.23-.09-.46-.17-.69-.25-.23-.08-.46-.17-.7-.24-.23-.07-.47-.12-.7-.18-.24-.06-.48-.13-.72-.18-.27-.05-.55-.09-.82-.13-.21-.03-.42-.07-.63-.09-.99-.1-1.98-.1-2.96,0-.21.02-.42.06-.63.09-.27.04-.55.07-.82.13-.24.05-.48.12-.72.18-.23.06-.47.11-.7.18-.24.07-.47.16-.7.24-.23.08-.46.16-.69.25-.22.09-.43.2-.64.3-.23.11-.46.21-.69.33-.2.11-.4.23-.6.35-.22.13-.44.26-.66.4-.22.15-.43.32-.65.48-.18.13-.36.25-.53.4-.38.31-.75.65-1.1,1L4.39,106.01c-5.86,5.86-5.86,15.36,0,21.21,2.93,2.93,6.77,4.39,10.61,4.39s7.68-1.46,10.61-4.39L101.61,51.21v185.43c0,8.28,6.72,15,15,15s15-6.72,15-15V51.21l76.01,76.01c2.93,2.93,6.77,4.39,10.61,4.39s7.68-1.46,10.61-4.39c5.86-5.86,5.86-15.36,0-21.21Z"/>
                       </svg>
                   </Link>
+                </div>
+              </div>
+            </li>
+            <li className="overflow-hidden h-[30px]">
+              <div className={`${showExp3 ? "translate-y-0" : "-translate-y-12"} transition-transform duration-300 flex flex-row gap-8`}>
+                <p className="min-w-[58px] text-[10px]">2017 - 2019</p>
+                <div>
+                  <p className="uppercase text-[10px] font-bold">BAC S</p>
+                    <p className="uppercase text-[10px]">Lycée Alain Chartier - Bayeux</p>
                 </div>
               </div>
             </li>
