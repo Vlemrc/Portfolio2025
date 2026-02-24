a815350ba48bc1fbf1f3b01fa86473d78df6a904
Victor Lemercier <victorlemercier.dev@gmail.com>
2026-02-24 16:18:14 +0100

    projects order

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -3,7 +3,74 @@ import { ProjectType } from '@/types/ProjectType';
 
 export const data: ProjectType[] = [
     {
-        id: 1,
+        id: 8,
+        title: "EDNA",
+        description: "Fashion-focused social app exploring outfits, clothing identity, and digital wardrobes. The project reimagines social posting by linking garments to looks, enabling discovery, personal vestiaires, and style-driven insights.",
+        image: "/images/edna.svg",
+        imagemobile: "/images/edna-mobile.svg",
+        stack: [
+            "React Native", "TailwindCSS", "Next.JS", "Railway", "Figma", "AI", "PS"
+        ],
+        year: 2026,
+        color: "rgb(255 255 255)",
+        bgcolor: "#6F55E6",
+        bordercolor: "rgba(255, 255, 255, 0.15)",
+        type: "Personal project",
+        slug: "edna",
+        github: "https://github.com/Vlemrc/edna",
+    },
+    {
+        id: 7,
+        title: "Neuronote",
+        description: "A friend made Neuronote, an AI-powered study assistant to help students prepare for exams more efficiently. I crafted the whole user experience and interface with Figma for his app.",
+        image: "/images/neuronote.png",
+        stack: [
+            "Figma, Next, Motion, Tailwind"
+        ],
+        year: 2026,
+        link: "https://neuronote.app/",
+        color: "#E83371",
+        bgcolor: "#F4F8FE",
+        bordercolor: "#E833711A",
+        type: "Neuronote",
+        slug: "neuronote"
+    },
+    {
+        id: 6,
+        title: "Notes AI",
+        description: "I made this Apple Notes clone with AI features for my Master's Degree project. I made a speech to present my project and a little video to introduce this new \"learning mode\". Ahead of Apple Intelligence.",
+        image: "/images/applenotes.jpg",
+        stack: [
+            "Next", "TailwindCSS", "GSAP", "Motion", "PostgreSQL"
+        ],
+        year: 2025,
+        color: "rgb(255 195 0)",
+        bgcolor: "rgb(40 40 40)",
+        bordercolor: "rgba(255, 195, 0, 0.15)",
+        type: "Master's Degree project",
+        slug: "notes-ai",
+        video: "/videos/NotesV3JET.mp4",
+        github: "https://github.com/Vlemrc/applenotes-ai"
+    },
+    {
+        id: 5,
+        title: "EightyTwo",
+        description: "Promotional website for Eighty Two. I aim to create a complete wordpress site to train, with CPTUI to register wines and events. A club for wine enthusiasts.",
+        image: "/images/eightytwo.png",
+        stack: [
+            "Wordpress", "PHP", "JS", "GSAP"
+        ],
+        year: 2025,
+        color: "rgb(255 255 255)",
+        bgcolor: "rgb(100 6 35)",
+        bordercolor: "rgba(255, 255, 255, 0.15)",
+        type: "Personal project",
+        slug: "eightytwo",
+        github: "https://github.com/Vlemrc/Ei8htytw2",
+        video: "/videos/video82.mov",
+    },
+    {
+        id: 4,
         title: "Les pétales obscurs",
         description: "I made this promotional website for a fictional clothing brand. The goal was to train with GSAP. Bloom in the shadows.",
         image: "/images/lespetalesobscurs.png",
@@ -21,57 +88,7 @@ export const data: ProjectType[] = [
         github: "https://github.com/Vlemrc/LesPetalesObscurs"
     },
     {
-        id: 2,
-        title: "Neuronote",
-        description: "A friend made Neuronote, an AI-powered study assistant to help students prepare for exams more efficiently. I crafted the whole user experience and interface with Figma for his app.",
-        image: "/images/neuronote.png",
-        stack: [
-            "Figma, Next, Motion, Tailwind"
-        ],
-        year: 2025,
-        link: "https://neuronote.app/",
-        color: "#E83371",
-        bgcolor: "#F4F8FE",
-        bordercolor: "#E833711A",
-        type: "Neuronote",
-        slug: "neuronote"
-    },
-    {
-        id: 5,
-        title: "Notes AI",
-        description: "I made this Apple Notes clone with AI features for my Master's Degree project. I made a speech to present my project and a little video to introduce this new \"learning mode\". Ahead of Apple Intelligence.",
-        image: "/images/applenotes.jpg",
-        stack: [
-            "Next", "TailwindCSS", "GSAP", "Motion", "PostgreSQL"
-        ],
-        year: 2025,
-        color: "rgb(255 195 0)",
-        bgcolor: "rgb(40 40 40)",
-        bordercolor: "rgba(255, 195, 0, 0.15)",
-        type: "Master's Degree project",
-        slug: "notes-ai",
-        video: "/videos/NotesV3JET.mp4",
-        github: "https://github.com/Vlemrc/applenotes-ai"
-    },
-    {
-        id: 6,
-        title: "Quiz BNP",
-        description: "A quiz to test your knowledge about your electricity consumptions. 4 days to create this quiz from scratch with Wordpress and ACF.",
-        image: "/images/quiz-habitat.jpg",
-        stack: [
-            "Wordpress", "PHP", "JS"
-        ],
-        year: 2024,
-        link: "https://www.observatoirecetelem-quiz.com/habitat-eu-2024",
-        color: "rgb(42 128 84)",
-        bgcolor: "rgb(238 238 238)",
-        bordercolor: "rgba(42, 128, 84, 0.15)",
-        type: "BNP Paribas",
-        slug: "quiz-bnp",
-        video: "/videos/quizbnp.mov",
-    },
-    {
-        id: 4,
+        id: 3,
         title: "Bimota",
         description: "Webflow site to promote Bimota motos. My first project with webflow in my previous experience in Altavia Disko.",
         image: "/images/bimota.jpg",
@@ -88,43 +105,26 @@ export const data: ProjectType[] = [
         video: "/videos/bimota.mov",
     },
     {
-        id: 3,
-        title: "EightyTwo",
-        description: "Promotional website for Eighty Two. I aim to create a complete wordpress site to train, with CPTUI to register wines and events. A club for wine enthusiasts.",
-        image: "/images/eightytwo.png",
+        id: 2,
+        title: "Quiz BNP",
+        description: "A quiz to test your knowledge about your electricity consumptions. 4 days to create this quiz from scratch with Wordpress and ACF.",
+        image: "/images/quiz-habitat.jpg",
         stack: [
-            "Wordpress", "PHP", "JS", "GSAP"
+            "Wordpress", "PHP", "JS"
         ],
-        year: 2025,
-        color: "rgb(255 255 255)",
-        bgcolor: "rgb(100 6 35)",
-        bordercolor: "rgba(255, 255, 255, 0.15)",
-        type: "Personal project",
-        slug: "eightytwo",
-        github: "https://github.com/Vlemrc/Ei8htytw2",
-        video: "/videos/video82.mov",
+        year: 2024,
+        link: "https://www.observatoirecetelem-quiz.com/habitat-eu-2024",
+        color: "rgb(42 128 84)",
+        bgcolor: "rgb(238 238 238)",
+        bordercolor: "rgba(42, 128, 84, 0.15)",
+        type: "BNP Paribas",
+        slug: "quiz-bnp",
+        video: "/videos/quizbnp.mov",
     },
     {
-        id: 7,
-        title: "EDNA",
-        description: "Fashion-focused social app exploring outfits, clothing identity, and digital wardrobes. The project reimagines social posting by linking garments to looks, enabling discovery, personal vestiaires, and style-driven insights.",
-        image: "/images/edna.svg",
-        imagemobile: "/images/edna-mobile.svg",
-        stack: [
-            "React Native", "TailwindCSS", "Next.JS", "Railway", "Figma", "AI", "PS"
-        ],
-        year: 2026,
-        color: "rgb(255 255 255)",
-        bgcolor: "#6F55E6",
-        bordercolor: "rgba(255, 255, 255, 0.15)",
-        type: "Personal project",
-        slug: "edna",
-        github: "https://github.com/Vlemrc/edna",
-    },
-    {
-        id: 8,
+        id: 1,
         title: "Fondation Sport Ambition",
-        description: "The project focuses on showcasing the foundation’s mission, programs, and supported athletes through a clear, accessible, and impact-driven web experience.",
+        description: "The project focuses on showcasing the foundation's mission, programs, and supported athletes through a clear, accessible, and impact-driven web experience.",
         image: "/images/fsa.png",
         stack: [
             "Wordpress", "ACF", "PHP", "JS", "GSAP"
